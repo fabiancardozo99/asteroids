b621a13b0bf5ddbf76cecc1426ec8972b45afe1e
Fabian <fabiancardozo99@gmail.com>
2025-01-27 21:29:20 -0300

    game finished

--- circleshape.py
+++ circleshape.py
@@ -12,6 +12,12 @@ class CircleShape(pygame.sprite.Sprite):
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
+    
+    def check_collision(self, circle_shape):
+        distance = self.position.distance_to(circle_shape.position)
+        if (self.radius + circle_shape.radius) >= distance:
+            return True
+        return False
 
     def draw(self, screen):
         # sub-classes must override
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
+import sys
 from constants import *
-from player import Player
+from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
@@ -14,10 +18,16 @@ def main():
     # Groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(x, y)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -26,6 +36,16 @@ def main():
 
         for p in updatable:
             p.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                sys.exit()
+        
+            for bullet in shots:
+                if asteroid.check_collision(bullet):
+                    asteroid.split()
+                    bullet.kill()
 
         # player.update(dt)
         screen.fill("black")
